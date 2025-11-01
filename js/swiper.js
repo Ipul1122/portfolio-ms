ff9a8ad7ae5e18f6d3883d45927be46c8e978a66
muhammad syaifulloh <skyfoxmarket@gmail.com>
2025-11-02 00:19:25 +0700

    update scrollbar

--- js/swiper.js
+++ js/swiper.js
@@ -1,28 +1,37 @@
-     document.addEventListener('DOMContentLoaded', () => {
-            const swiper = new Swiper('.project-swiper', {
-                // Konfigurasi Swiper
-                loop: true,
-                slidesPerView: 1,
-                spaceBetween: 30,
-                
-                // Navigasi (tombol panah)
-                navigation: {
-                    nextEl: '.swiper-button-next',
-                    prevEl: '.swiper-button-prev',
-                },
+document.addEventListener('DOMContentLoaded', () => {
+    const swiper = new Swiper('.project-swiper', {
+        // Konfigurasi Swiper
+        loop: true,
+        slidesPerView: 1,
+        spaceBetween: 30,
+        
+        // Aktifkan gesture geser
+        simulateTouch: true,
+        allowTouchMove: true,
+        touchRatio: 1,
+        touchAngle: 45,
+        grabCursor: true,
+        
+        // Scrollbar
+        scrollbar: {
+            el: '.swiper-scrollbar',
+            draggable: true,
+            dragSize: 'auto',
+            snapOnRelease: true,
+        },
 
-                // Breakpoints (agar responsif)
-                breakpoints: {
-                    // ketika lebar >= 768px (md)
-                    768: {
-                        slidesPerView: 2,
-                        spaceBetween: 30
-                    },
-                    // ketika lebar >= 1024px (lg)
-                    1024: {
-                        slidesPerView: 3,
-                        spaceBetween: 30
-                    }
-                }
-            });
-        });
+        // Breakpoints (agar responsif)
+        breakpoints: {
+            // ketika lebar >= 768px (md)
+            768: {
+                slidesPerView: 2,
+                spaceBetween: 30
+            },
+            // ketika lebar >= 1024px (lg)
+            1024: {
+                slidesPerView: 3,
+                spaceBetween: 30
+            }
+        }
+    });
+});
